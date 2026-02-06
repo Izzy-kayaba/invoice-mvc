18485eba161c5b0f5d0e6271cece8d44888ffea4
Izzy-kayaba <israelkayaba@gmail.com>
2026-02-06 19:32:12 +0200

    initial commit

---
+++ IzzyInvoice/Dtos/InvoiceLineDto.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace IzzyInvoice.Dtos;
+
+public record InvoiceLineDto
+{
+    [Required]
+    [StringLength(200)]
+    public string Description { get; init; } = default!;
+
+    [Range(1, int.MaxValue)]
+    public int Quantity { get; init; }
+
+    [Range(0.01, double.MaxValue)]
+    public decimal UnitPrice { get; init; }
+
+    public decimal LineTotal => Quantity * UnitPrice;
+}
